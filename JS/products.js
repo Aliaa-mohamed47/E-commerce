910a7ad67907ac356095b65fdeb2b27d503f138c
Rawan Essam <rawanessamm1@gmail.com>
2025-07-14 01:58:03 +0300

    homepage

--- JS/products.js
+++ JS/products.js
@@ -1,1 +1,43 @@
 // File: products.js - Display products - Fatma & Rawan
+
+// go to top btn
+let btn = document.getElementById("backToTop")
+btn.onclick = () => {
+    window.scrollTo ({
+        top: 0,
+        behavior: "smooth",
+    })
+}
+window.onscroll = () => {
+  if (window.scrollY > 300) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+
+// subscribe form validation
+const form = document.getElementById("subscribeForm");
+const emailInput = document.getElementById("emailInput");
+const emailMessage = document.getElementById("emailMessage");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();      // prevent refresh after clicking 'submit'
+  const email = emailInput.value.trim();
+  const valid = email.includes("@") && email.includes(".com");
+  if (email == "") {
+    emailMessage.textContent = "Please enter your email.";
+    emailMessage.style.color = "red";
+  } else if (!valid) {
+    emailMessage.textContent = "Enter a valid email.";
+    emailMessage.style.color = "red";
+  } else {
+    emailMessage.textContent = "Thank you for subscribing!";
+    emailMessage.style.color = "#1ca429ff";
+    emailInput.value = "";    // ba3d ma ye3mel submit yeshel el email
+  }
+  setTimeout(() => {
+      emailMessage.textContent = "";
+    }, 4000);
+});
